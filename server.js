1f7cfcc07fc722d7ad38b3196e01f0fe2ade00b1
jiiaa <jukkaa@live.com>
2019-03-20 10:58:43 +0200

    Kokeillaan audiofilen postausta clientista /Jukka

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-var express = require('express');
-var app = express();
-var cors = require('cors');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var parser = bodyParser.urlencoded({ extended: true });
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const parser = bodyParser.urlencoded({ extended: true });
 
-var AWS = require('aws-sdk');
-var uuid = require('uuid')
+const AWS = require('aws-sdk');
+const uuid4 = require('uuid4');
+const multer = require('multer');
+const store = multer.memoryStorage();
+const upload = multer({ storage: store })
+
+const logger = (req, res, next) => {
+    let d = new Date();
+    let options = { hour12: false };
+    let date = d.toLocaleString('fi-FI', options);
+    console.log(req.method + "@" + req.originalUrl + " / " + date);
+    next();
+}
+
+const API_VERSION = '2006-03-01';
+const BUCKET_NAME = 'pitchasong';
 
 const Pool = require('pg').Pool;
 const conopts = {
@@ -21,7 +35,26 @@ const pool = new Pool(conopts);
 
 app.use(cors());
 app.use(express.static('public'));
+app.use(logger);
 
+//Lisätään clientin POST-pyynnössä lähetämä äänitiedosto S3 Buckettiin
+app.post('/bucket', upload.single('audiofile'), (req, res) => {
+    console.log("req: ", req.file);
+    
+    // let fileFormat = req.file.mimetype.split("/")[1].trim();
+    // let fileName = req.file.fieldname + uudi4() + "." + fileFormat;
+    // let objectParams = { Bucket: BUCKET_NAME, Key: fileName, Body: req.file.buffer, ContentType: req.file.mimetype };
+
+    // let uploadPromise = new AWS.S3({ apiVersion: API_VERSION }).putObject(objectParams).promise();
+    // uploadPromise
+    // .then(data => {
+    //     console.log("Uploaded data to " + BUCKET_NAME + "/" + fileName);
+    // })
+    // .catch(error => console.error(error));
+    res.send("Hiio-hoi!");
+});
+
+// Tallentaa tekstikentän Postgresiin
 app.get('/api', function (req, res) {
     console.log("GET api");
 
@@ -41,6 +74,7 @@ app.get('/api', function (req, res) {
     res.json(response);
 });
 
+//Lisää kovakoodatun tekstitiedoston S3 Buckettiin, kun frontista klikataan painiketta
 app.post('/s3', parser, function (req, res) {
     let d = new Date();
     var options = { hour12: false };
@@ -65,29 +99,6 @@ app.post('/s3', parser, function (req, res) {
                 console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
                 res.send("ok/ ", data);
                 return;
-            };
-        });
-});
-
-app.get('/s3', function (req, res) {
-    let d = new Date();
-    var options = { hour12: false };
-    let date = d.toLocaleString('fi-FI', options);
-    console.log("GET S3", date);
-
-    let bucketName = "pitchasong";
-    let keyName = req.query.title + ".txt";
-    let objectParams = { Bucket: bucketName, Key: keyName };
-    console.log("Params: ", objectParams);
-    var downloadPromise = new AWS.S3().getObject(objectParams).promise();
-    downloadPromise.then(
-        function (err, data) {
-            if (err) {
-                console.error(err);
-                res.send("Error: ", err);
-            } else {
-                console.log("Successfully downloaded data from " + bucketName + "/" + keyName);
-                res.send("ok/ ", data);
             };
         });
 });
