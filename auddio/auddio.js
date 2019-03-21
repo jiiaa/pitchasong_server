42db6462e63554360e13b8db1d6b117120065aa2
Juhanir <juhani.riisio@gmail.com>
2019-03-21 12:28:52 +0200

    juukeli

--- auddio/auddio.js
+++ auddio/auddio.js
@@ -2,14 +2,20 @@ const fetch = require('node-fetch');
 
 const getHummingResults = async audio_url => {
     try {
-        let result = await fetch('https://api.audd.io/recognizeWithOffset/?url=' + audio_url);
+        let result = await fetch('https://audd.p.rapidapi.com/recognizeWithOffset/?url=' + audio_url, {
+            method: 'GET',
+            headers: {
+                Accept:'application/json',
+                'X-RapidAPI-Key': ''
+            }
+        };
         if (!result.ok) {
             throw new Error('Audd.io humming query failed with statuscode ' + result.status);
         }
         let jsonRes = await result.json();
         return jsonRes;
     } catch (error) {
-        return error.message;
+        throw error;
     }
 };
 
--- routes/humroute.js
+++ routes/humroute.js
@@ -38,7 +38,7 @@ const mockAnswerFalse = JSON.stringify({ status: 'success', result: null });
 // <request: hummed audiofile (parsed with multer)>
 // <response: Audd.io API response or failure message>
 router.get('/', (req, res) => {
-    res.status(200).json({ success: true, message: mockAnswerTrue });
+    res.status(200).json({ success: true, message: mockAnswerFalse });
     // auddio
     //     .getHummingResults(bullshit)
     //     .then(response => {
@@ -48,20 +48,23 @@ router.get('/', (req, res) => {
     // res.end();
 });
 
-router.post('/', upload.single('audiofile'), (req, res) => {
+router.post('/', upload.single('audiofile'), async (req, res) => {
     console.log('req: ', req.file);
 
     let fileFormat = req.file.mimetype.split('/')[1].trim();
     let fileName = req.file.fieldname + uuid4() + '.' + fileFormat;
     let objectParams = { Bucket: constants.BUCKET_NAME, Key: fileName, Body: req.file.buffer, ContentType: req.file.mimetype };
 
-    let uploadPromise = new AWS.S3({ apiVersion: constants.API_VERSION }).putObject(objectParams).promise();
-    uploadPromise
-        .then(data => {
-            console.log('Uploaded data to ' + constants.BUCKET_NAME + '/' + fileName);
-        })
-        .catch(error => console.error(error));
-    res.send(constants.BUCKET_URL + fileName);
+    try {
+        let uploadResult = await new AWS.S3({ apiVersion: constants.API_VERSION }).putObject(objectParams).promise();
+        let filePath = constants.BUCKET_URL + '/lastchristmas.mp3';
+        // let filePath = constants.BUCKET_URL + '/' + fileName;
+        console.log('Uploaded data to ' + filePath);
+        let humResults = await auddio.getHummingResults(filePath);
+        res.status(200).json({ success: true, message: humResults });
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message });
+    }
 });
 
 module.exports = router;
