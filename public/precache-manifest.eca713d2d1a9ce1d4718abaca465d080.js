1f1a89a0e4aec1099e6ee8428acc005575694fd7
Juhanir <juhani.riisio@gmail.com>
2019-03-22 16:04:12 +0200

    trying to build frontend

---
+++ public/precache-manifest.eca713d2d1a9ce1d4718abaca465d080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6e71b25ac58d49e86fd6",
+    "url": "/static/js/main.fe5fc522.chunk.js"
+  },
+  {
+    "revision": "870fdecf8fd30f0e4434",
+    "url": "/static/js/2.5de09d99.chunk.js"
+  },
+  {
+    "revision": "6e71b25ac58d49e86fd6",
+    "url": "/static/css/main.b72951c4.chunk.css"
+  },
+  {
+    "revision": "870fdecf8fd30f0e4434",
+    "url": "/static/css/2.739c2a13.chunk.css"
+  },
+  {
+    "revision": "bf7235697098d63028843d018327b663",
+    "url": "/index.html"
+  }
+];
