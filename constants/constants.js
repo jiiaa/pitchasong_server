4845c43a688a8b7107c1716fe274db86aa1a0db9
jiiaa <jukkaa@live.com>
2019-03-22 14:19:34 +0200

    Bucket JA /Jukka

--- constants/constants.js
+++ constants/constants.js
@@ -1,8 +1,8 @@
 // require('dotenv').config({ path: '.env.development' });
 
 const API_VERSION = '2006-03-01';
-const BUCKET_NAME = process.env.BUCKET_JR;
-const BUCKET_URL = process.env.BUCKET_URL_JR;
+const BUCKET_NAME = process.env.BUCKET_JA;
+const BUCKET_URL = process.env.BUCKET_URL_JA;
 const API_KEY = process.env.API_KEY;
 const SPOTIFY_AUTH = process.env.SPOTIFY_AUTH;
 
