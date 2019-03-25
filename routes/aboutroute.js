e29f36275427f58acc5ad10feb49eb5eb5e7baa6
jiiaa <jukkaa@live.com>
2019-03-25 16:17:33 +0200

    Trying to fix bugs /Jukka

--- routes/aboutroute.js
+++ routes/aboutroute.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const dbservice = require('../database/dbservice')
 
 router.get('/', (req, res) => {
+    console.log("GET About");
     dbservice.getAbout(response => {
         console.log(response);
         res.json(response)
